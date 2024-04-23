12a4a56e0f4f2aa3d2f2722d4abb0e4221a8cc96
Leona Hammelrath <50407361+leona-ha@users.noreply.github.com>
2024-04-23 15:28:13 +0200

    update pages

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # configuration file for the TIKI project
 
-datapath = "/Users/leonahammelrath/FU_Psychoinformatik/Github/tiki_public/data"
+datapath = "/Users/leonahammelrath/FU_Psychoinformatik/Github/tiki_code/data/"
 proj_sheet = "1z8LZJBBMzzAmiXIS47X8SLk-zSMwDIXSKPit4IlmfuE"
